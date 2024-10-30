640668b5fd11a4ff4686ee83c76a12c8a047bb04
Michael <michaelargueta79@gmail.com>
2024-10-30 19:05:52 -0300

    Practica 9

---
+++ practica09.kt
@@ -0,0 +1,37 @@
+fun fibonacci(n: Int): Int {
+    return if (n <= 1) {
+        n
+    } else {
+        fibonacci(n-1) + fibonacci(n-2)
+    }
+}
+
+fun sumaRecursiva(n: Int) {
+    return if (n: <= 1) {
+        1
+    } else {
+        n + sumaRecursiva(n-1)
+    }
+}
+
+fun sumaLista(lista: List<Int>): Int {
+    return if (lista.isEmpty()) {
+        0
+    } else {
+        lista[0] + sumaLista(lista.drop(1))
+    }
+}
+
+fun main(a rgs: Array<String>) {
+    //Ejemplo de Fibonacci
+    val numero = 15
+    val numeroLista = listOf(1, 2, 3, 4)
+    println("\nEjemplo 1")
+    println("Finocci de $numero es: ${fibonacci(numero)}")
+
+    println("\nEjemplo 2")
+    println("La suma de 1 a $numero es: ${sumaRecursiva(numero)}")
+
+    println("\nEjemplo 3")
+    println("La suma de la lista es: ${sumaLista(numerosLista)}")
+}
